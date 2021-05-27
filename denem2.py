541951a711bd8cd399cbbde3b684a73122416466
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-05-27 18:37:23 +0300

    Add files via upload

---
+++ denem2.py
@@ -0,0 +1,10 @@
+a = 50
+s = 60 
+d = 40 
+e = 50
+if a > d :
+    print (" A harfi büyüktür , D harfinden ")
+if d < a :
+    print(" D harfi küçüktür , A harfinden ")
+if e == a :
+    print("E harfi ile A harfi eşittir ")
